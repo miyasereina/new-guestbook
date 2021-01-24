e072fc706275bac911854f773870e0fdbbb006ea
miyasereina <74105720+miyasereina@users.noreply.github.com>
2021-01-24 18:03:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask,request,jsonify,session,render_template
 import pymysql
+from flask import Flask, request, jsonify, session, render_template
+from gevent import pywsgi
 
 app=Flask(__name__)
 app.secret_key='123456'
 
-con=pymysql.connect(user='root',passwd='reina0526', charset='utf8')
+
+con=pymysql.connect(user='root',password='reina0526', charset='utf8')
 cursor=con.cursor()
 cursor.execute("create database if not exists test character set utf8;")
 cursor.execute("use test")
@@ -16,62 +18,55 @@ cursor.execute("create table if not exists guestbook"
 @app.route('/',methods=['GET'])
 def board():
     value=cursor.fetchall()
-    cursor.close()
-    con.close()
     board=jsonify(value)
     return board
 
 @app.route('/guestbook',methods=['POST'])
 def write():
     data=request.get_json(force=True)
-    username=session['usesrname']
+    username=session.get('username')
     if username is None:
         raise HttpError(401, '请先登录')
     content=data.get('content')
-    cursor.execute('insert into `guestbook`(`content`,`username`,`post_time`) values(%s,%s,%s,%s)',
-                   (content,username,'now()'))
+    cursor.execute('insert into `guestbook`(`content`,`username`,`post_time`) values(%s,%s,now())',
+                   (content,username))
     con.commit()
-    cursor.close()
-    con.close()
     session['content']=content
     return "发表成功"
 
 @app.route('/guestbook/content',methods=["PUT"])
 def correct():
     data=request.get_json(force=True)
-    username=session['username']
-    content=session['content']
-    content2=data.get('content')
+    username=session.get('username')
+    content=session.get('content')
+    content2=data.get('content2')
     if username is None:
         raise HttpError(401, '请先登录')
-    cursor.execute('update `guestbook` set `content`=%s,last_correct=%s where username=%s and content=%s',
-                   (content,'now()',username,content2))
+    cursor.execute('update `guestbook` set `content`=%s,last_correct=now() where username=%s and content=%s',
+                   (content2,username,content))
     con.commit()
-    cursor.close()
-    con.close()
+    session['content']=content2
     return '修改成功'
 
-@app.route('/guestbook/deletion',methods=['DELETTE'])
+@app.route('/guestbook/deletion',methods=['DELETE'])
 def deletion():
-    content=session['content']
-    username=session['username']
+    content=session.get('content')
+    username=session.get('username')
     if username is None:
         raise HttpError(401, '请先登录')
-    cursor.execute('delete `guestbook` set `content`=%s,last_correct=%s where username=%s and content=%s',(content, 'now()', username,content))
+    cursor.execute('delete from`guestbook` where username=%s and content=%s',(username,content))
     con.commit()
-    cursor.close()
-    con.close()
     return '删除成功'
 
 @app.route('/user', methods=['POST','GET'])
 def register():
-    data=request.get_jsom(force=True)
+    data=request.get_json(force=True)
     username=data.get('username')
     nickname=data.get('nickname')
     password=data.get('password')
     sex=data.get("sex")
     cursor.execute('select count(*) from `user` where `username`=%s',(username,))
-    count=cursor.fetchall()
+    count=cursor.fetchone()
     if count[0]>=1:
         raise HttpError(409, '用户名已存在')
     if username is None:
@@ -84,19 +79,15 @@ def register():
         raise HttpError(400, "请输入sex")
     cursor.execute('insert into `user`(`username`,`nickname`,`password`,`sex`) values (%s, %s,%s,%s)',(username, nickname, password, sex))
     con.commit()
-    cursor.close()
-    con.close()
     return render_template('login.html')
 
 @app.route('/me',methods=['GET'])
 def my_information():
-    username=session['username']
+    username=session.get('username')
     if username is None:
         raise HttpError(401, "请先登录")
     cursor.execute('select `username`, `nickname`,`sex` from `user` where `username`=%s', (username,))
     data=cursor.fetchall()
-    cursor.close()
-    con.close()
     response=jsonify(data)
     return response
 
@@ -112,7 +103,7 @@ def login():
     values=cursor.fetchone()
     if values is None:
         raise HttpError(402, '用户名或密码错误')
-    if username!=values[0] or password!=values[2]:
+    if username!=values[0] or password!=values[1]:
         raise HttpError(402, '用户名或密码错误')
     session['username']=username
     session['password']=password
@@ -122,17 +113,18 @@ def login():
 @app.route('/users/username', methods=['PUT'])
 def change_username():
     data=request.get_json(force=True)
-    username=session['username']
+    username=session.get('username')
+    print(username)
     if username is None:
         raise HttpError(401, '请先登录')
-    cursor.execute('select count(*) from `users` where `username`=%s', (username,))
+    username2=data.get('username2')
+    cursor.execute('select count(*) from `user` where `username`=%s', (username2,))
     count=cursor.fetchone()[0]
     if count>=1:
         raise HttpError(409, '用户名已存在')
-    cursor.execute('update `users` set `username`=%s where username=%s', (session.get('username'), username))
+    cursor.execute('update `user` set `username`=%s where username=%s', (username2, username))
     con.commit()
-    cursor.close()
-    con.close()
+    session['username']=username2
     return '修改用户名成功'
 
 
@@ -145,8 +137,7 @@ def change_password():
         raise HttpError(401, '请先登录')
     cursor.execute('update `user` set `password`=%s where username=%s',(password,username))
     con.commit()
-    cursor.close()
-    con.close()
+    session['password']=password
     return '修改密码成功'
 
 class HttpError(Exception):
@@ -165,5 +156,7 @@ def handle_http_error(error):
     response = jsonify(error.to_dict())
     response.status_code = error.status_code
     return response
+
 if __name__ == '__main__':
-    app.run()
+    server=pywsgi.WSGIServer(('0.0.0.0',8000),app)
+    server.serve_forever()
